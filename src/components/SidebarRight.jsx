ff3501df2a48d3ae687030409a8710550f1216c3
Deepakkarki24 <ideepakkarki@gmail.com>
2024-02-07 21:49:45 +0530

    donee now🔥

--- src/components/SidebarRight.jsx
+++ src/components/SidebarRight.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import styles from "../components/SidebarRight.module.css";
 import TrendingEvent from "./TrendingEvent";
-import {
-  TwitterTimelineEmbed,
-  TwitterShareButton,
-  TwitterFollowButton,
-  TwitterHashtagButton,
-} from "react-twitter-embed";
 
 import SearcIcon from "@mui/icons-material/Search";
 
@@ -32,41 +26,29 @@ function SidebarRight({ subsTitle, subsContent }) {
 
       {/* happening area */}
       <div className={`${styles.box_style} ${styles.happening}`}>
-        <h3 className={styles.title}>Post & Events</h3>
+        <h3 className={styles.title}>#astags for You !</h3>
 
         <div className={styles.trending_events_box}>
-          <div className={styles.timelineEmbed}>
-            <TwitterTimelineEmbed
-              sourceType="profile"
-              screenName="deepak_here24"
-              options={{ height: 400 }}
-              theme="dark"
-            />
-          </div>
-          <div className={styles.follow_embed}>
-            <TwitterFollowButton screenName={"deepak_here24"} />
-          </div>
-          <div className={styles.hastagEmbed}>
-            <TwitterHashtagButton tag={"react"} />
-            <TwitterHashtagButton tag={"twitter"} />
-            <TwitterHashtagButton tag={"clone"} />
-          </div>
-          <div className={styles.shareEmbed}>
-            <TwitterShareButton
-              className={styles.tweetButton}
-              url={""}
-              options={{ text: "#reactjs is awesome", via: "deepak_here24" }}
-            />
-          </div>
-
-          {/* <TwitterHashtagButton tag={"cybersecurity"} /> */}
           {/* Trending Event component */}
           <TrendingEvent
             trendingTop={"Trending in India"}
-            trendingMiddle={"#ReactJs"}
+            trendingMiddle={"#React🔥"}
             trendingBottom={"Trending with "}
-            trendingBottomBlue={"#ReactJs"}
+            trendingBottomBlue={"#ReactJS"}
           />
+          <TrendingEvent
+            trendingTop={"Checkout my LinkedIn"}
+            trendingMiddle={"#ConnectWithMe"}
+            trendingBottom={"Deepak Karki 👈🏻 on "}
+            trendingBottomBlue={"#LinkedIn"}
+          />
+          <TrendingEvent
+            trendingTop={"Give Your Feedback as post"}
+            trendingMiddle={"FeedBack with Image or Gif 😎"}
+            trendingBottom={"Post your Feedback"}
+            trendingBottomBlue={"#showlove"}
+          />
+
           {/* Trending Event component */}
         </div>
         <a href="#">Show more</a>
--- src/components/TrendingEvent.jsx
+++ src/components/TrendingEvent.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import styles from "../components/SidebarRight.module.css";
 import MoreHoriz from "@mui/icons-material/MoreHoriz";
 
-function TrendingEvent({ trendingTop, trendingBottom, trendingBottomBlue }) {
+function TrendingEvent({
+  trendingTop,
+  trendingBottom,
+  trendingMiddle,
+  trendingBottomBlue,
+}) {
   return (
     <div className={styles.trending_event}>
       <div className="trending_context">
         <h4 className={styles.fadeTitle}>{trendingTop}</h4>
-        <h5 className={styles.boldTitle}>{trendingTop}</h5>
+        <h5 className={styles.boldTitle}>{trendingMiddle}</h5>
         <h4 className={styles.fadeTitle}>
           {trendingBottom}
           <span className="text-primary fw-normal">{trendingBottomBlue}</span>
